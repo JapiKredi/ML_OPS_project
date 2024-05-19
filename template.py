8e672208f8d4831256f4e58f975251fddd9e9eee
JapiKredi <jasper.bongers@yahoo.com>
2024-05-19 12:41:13 +0800

    Update code

--- template.py
+++ template.py
@@ -5,5 +5,8 @@ from pathlib import Path
 list_of_files = [
     ".github/workflows",
     "src.__init__.py",
-    "src/components"
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    
 ]
+
